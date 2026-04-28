410334769ce8ad1df9655bfad7e9b23f67fb6486
엄송현 <z9901z@naver.com>
2026-04-28 11:02:08 +0900

    fix: request url https 로 수정

--- gemmark/FE/src/shared/lib/axios.ts
+++ gemmark/FE/src/shared/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ??
-    'http://k14s307.p.ssafy.io:8000/api/v1',
+    'https://k14s307.p.ssafy.io:8000/api/v1',
   timeout: 30_000, // 30초
 })
