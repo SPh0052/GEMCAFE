9b742e9f28852942e737e600d471ea07806187b7
엄송현 <z9901z@naver.com>
2026-04-28 11:09:02 +0900

    fix: port 번호 변경

--- gemmark/FE/src/shared/lib/axios.ts
+++ gemmark/FE/src/shared/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ??
-    'https://k14s307.p.ssafy.io:8000/api/v1',
+    'https://k14s307.p.ssafy.io/api/v1',
   timeout: 30_000, // 30초
 })
