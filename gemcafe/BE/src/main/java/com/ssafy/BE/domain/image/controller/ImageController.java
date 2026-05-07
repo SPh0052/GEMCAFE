38cbd3e8b96b69b3ac72586afb590f32162c20e0
송은혁 <seh8145@naver.com>
2026-05-07 09:34:09 +0900

    [S14P31S307-155] feat: [BE] 이미지 업로드 API 경로 수정

--- gemcafe/BE/src/main/java/com/ssafy/BE/domain/image/controller/ImageController.java
+++ gemcafe/BE/src/main/java/com/ssafy/BE/domain/image/controller/ImageController.java
@@ -1,7 +1,7 @@
-package com.ssafy.BE.domain.photo.controller;
+package com.ssafy.BE.domain.image.controller;
 
-import com.ssafy.BE.domain.photo.dto.PhotoUploadResponse;
-import com.ssafy.BE.domain.photo.service.PhotoService;
+import com.ssafy.BE.domain.image.dto.ImageUploadResponse;
+import com.ssafy.BE.domain.image.service.ImageService;
 import com.ssafy.BE.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/photos")
+@RequestMapping("/api/v1/images")
 @RequiredArgsConstructor
-public class PhotoController {
+public class ImageController {
 
-    private final PhotoService photoService;
+    private final ImageService imageService;
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ApiResponse<PhotoUploadResponse> upload(
+    public ApiResponse<ImageUploadResponse> upload(
             @RequestParam("file") MultipartFile file
     ) {
-        PhotoUploadResponse data = photoService.upload(file);
-        return ApiResponse.ok("사진 업로드 성공", data);
+        ImageUploadResponse data = imageService.upload(file);
+        return ApiResponse.ok("이미지 업로드 성공", data);
     }
 }
