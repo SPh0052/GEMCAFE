2c594e11be466c4ede62c3a5022603403a608fa6
송은혁 <seh8145@naver.com>
2026-05-07 11:20:17 +0900

    [S14P31S307-155] refactor: [BE] CORS 경로 포트 추가

--- gemcafe/BE/src/main/java/com/ssafy/BE/global/config/CorsConfig.java
+++ gemcafe/BE/src/main/java/com/ssafy/BE/global/config/CorsConfig.java
@@ -15,11 +15,25 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
+                "https://localhost:5173",
+                "http://localhost:5174",
+                "https://localhost:5174",
+                "http://localhost:5175",
+                "https://localhost:5175",
                 "http://localhost:3000",
+                "https://localhost:3000",
                 "http://localhost:8002",
+                "https://localhost:8002",
                 "http://127.0.0.1:5173",
                 "http://127.0.0.1:3000",
                 "http://127.0.0.1:8002",
+                "https://127.0.0.1:5173",
+                "https://127.0.0.1:3000",
+                "https://127.0.0.1:8002",
+                "http://localhost:8000",
+                "https://localhost:8000",
+                "http://localhost:8001",
+                "https://localhost:8001",
                 "https://k14s307.p.ssafy.io",
                 "http://k14s307.p.ssafy.io"
         ));
